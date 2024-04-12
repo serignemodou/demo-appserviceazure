329291f179dce71ed25af61e510b2587a30cf3d4
serignemodou <modoudjuff@gmail.com>
2024-04-12 16:55:14 +0200

    feat: appinsights deploy apps

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -5,23 +5,39 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+import com.microsoft.applicationinsights.telemetry.SeverityLevel;
+import com.microsoft.applicationinsights.TelemetryClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	private static final TelemetryClient telemetryClient = new TelemetryClient();
+	Map<String, String> mp = new HashMap<>();
+	
 
 	@RequestMapping("/") 
 	public String home() { 
 		String username = "modou";
+		mp.put("Squad", "swap");
+		mp.put("Poste", "OPS");
 		logger.info("cs-username=" + username);
-		return "Logging Application Demo"; 
+		telemetryClient.trackTrace("User details", SeverityLevel.Information, mp);
+		return "Logging Application Demo V2"; 
 	}
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
