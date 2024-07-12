fe6219ce4c450469fba1afff6f29632aa23d4541
serignemodou <modoudjuff@gmail.com>
2024-07-12 15:17:41 +0200

    feat: deploy app to app service azure

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -3,10 +3,12 @@ package com.demo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.microsoft.applicationinsights.attach.ApplicationInsights; 
 
 @SpringBootApplication
+@RestController
 public class DemoApplication implements WebMvcConfigurer{
 
 	@RequestMapping("/") 
