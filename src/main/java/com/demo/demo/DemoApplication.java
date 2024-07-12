63e17fd67f600e607e55114ba281fb0bdf827fd8
serignemodou <modoudjuff@gmail.com>
2024-07-12 16:07:13 +0200

    feat: deploy app to app service azure

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.demo.demo;
 import java.io.IOException;
 import java.util.Enumeration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,15 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest; 
 
+
 @SpringBootApplication
 @RestController
 public class DemoApplication implements Filter{
 
-	    static final TelemetryClient telemetryClient = new TelemetryClient();
+	static final TelemetryClient telemetryClient = new TelemetryClient();
     RequestTelemetry requestTelemetry = new RequestTelemetry();
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException{
@@ -33,6 +38,8 @@ public class DemoApplication implements Filter{
             logHttpRequestHeaders(httpServletRequest);
         }
         chain.doFilter(request, response);
+		logger.info("Do filer called");
+		
     }
     
     private void logHttpRequestHeaders(HttpServletRequest request) {
@@ -46,6 +53,7 @@ public class DemoApplication implements Filter{
                 requestTelemetry.getProperties().put(headerName, headerValue);
             }
             System.out.println("Application version v01");
+			logger.info("Application version v01");
         }
     }
 
