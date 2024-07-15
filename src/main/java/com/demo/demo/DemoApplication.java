0482485c40af0ee659e8f928671f7e5938407513
serignemodou <modoudjuff@gmail.com>
2024-07-15 11:51:04 +0200

    feat: deploy app to app service azure

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class DemoApplication {
 	@RequestMapping("/") 
 	public String getRequestHeaders(HttpServletRequest request) {
-		return "Logging Application Demo V09"; 
+		return "Logging Application Demo V10"; 
 	}
 
 	public static void main(String[] args) {
