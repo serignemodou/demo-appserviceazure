aa3255ac6e5355c99a7730ca866de91a75770905
serignemodou <modoudjuff@gmail.com>
2024-07-18 14:08:12 +0200

    feat: deploy app to app service azure

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class DemoApplication {
 	@RequestMapping("/") 
 	public String getRequestHeaders(HttpServletRequest request) {
-		return "Logging Application Demo V28-rc"; 
+		return "Logging Application Demo V29"; 
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/demo/demo/FilterOtel.java
+++ src/main/java/com/demo/demo/FilterOtel.java
@@ -48,14 +48,13 @@ public class FilterOtel implements Filter {
             Enumeration<String> headers = request.getHeaders(headerName);
             while (headers.hasMoreElements()) {
                 String headerValue = headers.nextElement();
-              //  System.out.println("Header" + headerName + " = " + headerValue);
+                System.out.println("Header" + headerName + " = " + headerValue);
                 requestTelemetry.getProperties().put(headerName, headerValue);
                 //headersMap.put(headerName, headerValue);
             }
         }
        // telemetryClient.trackTrace("users details", SeverityLevel.Information, headersMap);
         telemetryClient.trackRequest(requestTelemetry);
-        telemetryClient.trackHttpRequest("demo", null, 7, "200", false);
       //  telemetryClient.flush();
     }
 }
