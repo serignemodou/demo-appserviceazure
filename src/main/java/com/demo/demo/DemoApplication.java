6b067352ab00fa95105942164efcecd43a34650c
serignemodou <modoudjuff@gmail.com>
2024-07-18 10:34:52 +0200

    feat: deploy app to app service azure

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class DemoApplication {
 	@RequestMapping("/") 
 	public String getRequestHeaders(HttpServletRequest request) {
-		return "Logging Application Demo V22-rc"; 
+		return "Logging Application Demo V23"; 
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/demo/demo/FilterOtel.java
+++ src/main/java/com/demo/demo/FilterOtel.java
@@ -17,7 +17,6 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
-import com.microsoft.applicationinsights.telemetry.SeverityLevel;
 
 @WebFilter(urlPatterns =  "/*")
 @Component
@@ -41,7 +40,6 @@ public class FilterOtel implements Filter {
     
     private void logHttpRequestHeaders(HttpServletRequest request) {
         RequestTelemetry requestTelemetry = new RequestTelemetry();
-        
 
         Enumeration<String> headerNames = request.getHeaderNames();
         Map<String, String> headersMap = new HashMap<>();
