ff0952e887c5409128579efe2b49d9a5197e9d44
serignemodou <modoudjuff@gmail.com>
2024-07-22 14:32:29 +0200

    feat: deploy app to app service azure

--- src/main/java/com/demo/demo/FilterOtel.java
+++ src/main/java/com/demo/demo/FilterOtel.java
@@ -14,7 +14,6 @@ import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
@@ -24,6 +23,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @WebFilter(urlPatterns = "/app/v1")
 @Component
+@ServletComponentScan
 public class FilterOtel implements Filter {
 
     static final TelemetryClient telemetryClient = new TelemetryClient();
