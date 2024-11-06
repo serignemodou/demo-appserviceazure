8d01b4a730d8a4735f47856792f1844e7bfc8442
serignemodou <modoudjuff@gmail.com>
2024-11-06 10:48:15 +0100

    feat: update pom xml config

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -15,12 +15,12 @@ public class DemoApplication {
 
 	@RequestMapping("/app/v1/postetravail/gestionnaire/tenants/historique")
 	public String appV1(){
-		return "tenant fouth path url, v5";
+		return "tenant fouth path url, v+6";
 	}
 
 	@RequestMapping("/app/v1/health-check")
 	public String appV2(){
-		return "tenant just path after the domain v5";
+		return "tenant just path after the domain v+6";
 	}
 	public static void main(String[] args) {
         ApplicationInsights.attach();
