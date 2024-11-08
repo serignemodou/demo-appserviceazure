20c8eaa50aea7a33afcaadea9e24d93c780085a2
serignemodou <modoudjuff@gmail.com>
2024-11-08 14:28:32 +0100

    feat: deploy sampling config

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -15,12 +15,12 @@ public class DemoApplication {
 
 	@RequestMapping("/app/v1/parcours/utilisateurs")
 	public String appV1(){
-		return "tenant fouth path url, v+16";
+		return "tenant fouth path url, v+17";
 	}
 
 	@RequestMapping("/app/v1/health-check")
 	public String appV2(){
-		return "tenant just path after the domain v+15";
+		return "tenant just path after the domain v+17";
 	}
 	public static void main(String[] args) {
         ApplicationInsights.attach();
